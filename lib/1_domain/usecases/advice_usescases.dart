cab0194a06fda1997c79253ad4e7bfc71716f2e2
Dean White <de4nwhit3@gmail.com>
2023-07-04 18:17:44 +0200

    added entity

--- lib/1_domain/usecases/advice_usescases.dart
+++ lib/1_domain/usecases/advice_usescases.dart
@@ -1,6 +1,9 @@
+import 'package:fourth_assignment/1_domain/entities/advice_entity.dart';
+
 class AdviceUseCases {
-  Future<String> getAdvice() async {
+  Future<AdviceEntity> getAdvice() async {
     await Future.delayed(const Duration(seconds: 3));
-    return 'Fake advice from use case future';
+    return const AdviceEntity(
+        advice: 'Fake advice from use case future 123', id: 1);
   }
 }
--- lib/2_application/pages/advice/cubit/small_advice_cubit.dart
+++ lib/2_application/pages/advice/cubit/small_advice_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fourth_assignment/1_domain/entities/advice_entity.dart';
 import 'package:fourth_assignment/1_domain/usecases/advice_usescases.dart';
 
 part 'small_advice_state.dart';
@@ -11,8 +12,8 @@ class SmallAdviceCubit extends Cubit<SmallAdviceState> {
   void requestAdvice() async {
     emit(AdvicerStateLoading());
     debugPrint("Loading state emitted");
-    final String advice = await adviceUseCases.getAdvice();
-    emit(AdvicerStateLoaded(advice: advice));
+    final AdviceEntity advice = await adviceUseCases.getAdvice();
+    emit(AdvicerStateLoaded(advice: advice.advice));
     debugPrint('Loaded fake advice');
     // emit(AdvicerStateError(errorMsg: 'Some dummy error message'));
   }
