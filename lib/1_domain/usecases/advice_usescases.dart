13108ec093165f0c718fbaaf2117dca744405a07
Dean White <de4nwhit3@gmail.com>
2023-07-05 23:10:35 +0200

    implemented failures

--- lib/1_domain/usecases/advice_usescases.dart
+++ lib/1_domain/usecases/advice_usescases.dart
@@ -1,9 +1,11 @@
+import 'package:dartz/dartz.dart';
 import 'package:fourth_assignment/1_domain/entities/advice_entity.dart';
+import 'package:fourth_assignment/1_domain/failures/failures.dart';
 
 class AdviceUseCases {
-  Future<AdviceEntity> getAdvice() async {
+  Future<Either<Failure, AdviceEntity>> getAdvice() async {
     await Future.delayed(const Duration(seconds: 3));
-    return const AdviceEntity(
-        advice: 'Fake advice from use case future 123', id: 1);
+    // return right(const AdviceEntity(advice: 'Fake advice', id: 1));
+    return left(CacheFailure());
   }
 }
--- lib/2_application/pages/advice/cubit/small_advice_cubit.dart
+++ lib/2_application/pages/advice/cubit/small_advice_cubit.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:fourth_assignment/1_domain/entities/advice_entity.dart';
 import 'package:fourth_assignment/1_domain/usecases/advice_usescases.dart';
 
+import '../../../../1_domain/failures/failures.dart';
+
 part 'small_advice_state.dart';
+
+const serverErrorMessage = 'server error';
+const cacheErrorMessage = 'cache failure';
+const defaultErrorMessage = 'default failure';
 
 class SmallAdviceCubit extends Cubit<SmallAdviceState> {
   SmallAdviceCubit() : super(SmallAdviceInitial());
@@ -12,9 +17,23 @@ class SmallAdviceCubit extends Cubit<SmallAdviceState> {
   void requestAdvice() async {
     emit(AdvicerStateLoading());
     debugPrint("Loading state emitted");
-    final AdviceEntity advice = await adviceUseCases.getAdvice();
-    emit(AdvicerStateLoaded(advice: advice.advice));
+    final failureOrAdvice = await adviceUseCases.getAdvice();
+
+    failureOrAdvice.fold(
+        (failure) =>
+            emit(AdvicerStateError(errorMsg: _mapFailureToMessage(failure))),
+        (advice) => emit(AdvicerStateLoaded(advice: advice.advice)));
     debugPrint('Loaded fake advice');
-    // emit(AdvicerStateError(errorMsg: 'Some dummy error message'));
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return serverErrorMessage;
+      case CacheFailure:
+        return cacheErrorMessage;
+      default:
+        return defaultErrorMessage;
+    }
   }
 }
